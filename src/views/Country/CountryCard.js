99d3bccbe977fd73c82ed59405e751f2771a854e
Nityam <nityam@grappus.com>
2020-05-20 00:51:43 +0530

    Debugger removed

--- src/views/Country/CountryCard.js
+++ src/views/Country/CountryCard.js
@@ -12,16 +12,14 @@ class CountryCard extends Component {
 
 
     fetchWeather = async (capital) => {
-        debugger
         let result = await fetch(`http://api.weatherstack.com/current?access_key=${WeatherStackApiAcessKey}&query=${capital}`).then(res => res.json()).then(res => { return res })
-        this.setState({ weatherData: result })
         if (result) {
+            this.setState({ weatherData: result })
             this.setState({ isLoading: true })
         }
     }
     render() {
-        const { data, weatherData,isLoading } = this.props;
-
+        const { data, weatherData, isLoading } = this.props;
         return (
             <>
                 <tr>
@@ -33,13 +31,18 @@ class CountryCard extends Component {
                         {data ? <img src={data.flag} style={{ maxWidth: "120px" }} alt="flag" /> : ""}
                     </td>
                     <td>
-                       { !isLoading?
-                        <span onClick={() => this.fetchWeather(data.capital)} >
-                            Capital Weather
-                         </span>:
-                         <WeatherCard data={weatherData} />
+                        {
+                            !isLoading ?
+                                (
+                                    <span onClick={() => this.fetchWeather(data.capital)} >
+                                        Capital Weather
+                                    </span>
+                                )
+                                : (
+                                    <WeatherCard data={weatherData} />
+                                )
 
-                    }
+                        }
                     </td>
 
                 </tr>
@@ -51,23 +54,25 @@ class CountryCard extends Component {
     }
 }
 
-function WeatherCard({data}) {
-     const { current } = data;
-     return (
-         <>
-             <p>Temperature {current ? current.temperature : ""}</p>
-             <p>Wind_Speed{current ? current.wind_speed : ""}</p>
-             <p>precip {current ? current.precip : ""}</p>
-             <p>
-                 { current.weather_icons.map((i)=>(
-                         <img src={i} alt=""/>
-                     ))
-                 }
-                 </p>
- 
-         </>
-     )
 
-}
 
 export default CountryCard;
+
+function WeatherCard({ data }) {
+    const { current } = data;
+    return (
+        <>
+            <p>Temperature {current ? current.temperature : ""}</p>
+            <p>Wind_Speed{current ? current.wind_speed : ""}</p>
+            <p>precip {current ? current.precip : ""}</p>
+            <p>
+                {current.weather_icons.map((i) => (
+                    <img src={i} alt="" />
+                ))
+                }
+            </p>
+
+        </>
+    )
+
+}
